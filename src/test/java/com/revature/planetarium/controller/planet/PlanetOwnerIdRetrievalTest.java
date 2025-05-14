df9ddf2af16bfe09a339323119459df4e3d63ec6
AlbertOh99 <ohalbert1999@gmail.com>
2025-05-14 01:54:21 -0400

    Fixed my planetowneridretrievaltest and added a json to facilitate that.

--- src/test/java/com/revature/planetarium/controller/planet/PlanetOwnerIdRetrievalTest.java
+++ src/test/java/com/revature/planetarium/controller/planet/PlanetOwnerIdRetrievalTest.java
@@ -4,6 +4,7 @@ import io.javalin.http.ContentType;
 import io.javalin.http.HttpStatus;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,25 +15,30 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 
 public class PlanetOwnerIdRetrievalTest {
 
     private Map<String, String> loginInfo;
     private int ownerId;
-
-
+    private String jsonBody;
 
     @Before
-    public void setup(){
+    public void setup() throws IOException {
         ownerId = 1;
         this.loginInfo = new HashMap<>();
         this.loginInfo.put("username", "Batman");
         this.loginInfo.put("password", "Iamthenight1939");
+
+        jsonBody = new String(Files.readAllBytes(Paths.get("src/test/java/com/revature/planetarium/controller/planet/retrievalData.json")));
+//        File test = new File(Paths.get("src/test/java/com/revature/planetarium/controller/planet/retrievalData.json"));
     }
 
 
@@ -49,8 +55,9 @@ public class PlanetOwnerIdRetrievalTest {
         given()
                 .cookies(cookies)
                 .when()
-                .get("planetarium/user/"+ ownerId +"/planet")
+                .get("planetarium/user/" + ownerId + "/planet")
                 .then()
-                .statusCode(200);
+                .statusCode(200).body(equalTo(jsonBody));
+
     }
 }
